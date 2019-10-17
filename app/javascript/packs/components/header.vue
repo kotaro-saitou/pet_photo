71de9a992d4c8dd9aa5a1c5282fe9f888699ec0d
kotaro <ks.s1230113@gmail.com>
2019-10-17 12:25:15 +0000

    ナビゲーションバー追加

---
+++ app/javascript/packs/components/header.vue
@@ -0,0 +1,14 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container">
+    <a class="navbar-brand " href="/">みんなのペット自慢</a>
+
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link" href="users/auth/twitter">Twitterログイン</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
